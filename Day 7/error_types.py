1d2d580eaaa5e7cffa44dfbd355a0b0d48517569
ShahxHussain <syedshahhussain19288@gmail.com>
2024-06-21 16:34:47 +0500

    code added

---
+++ Day 7/error_types.py
@@ -0,0 +1,32 @@
+# syntax or indentation erro
+# x = 5
+# if x › 3:
+print("x is a positive value")
+
+
+# # logical or human error
+# if x > 3:
+#
+print("y is a positive value")
+
+
+# runtime or module not found error
+import numpy as np
+print (np. sqrt (9))
+
+
+
+# Assignment
+# try to learn all about these errors
+
+# NameError 
+# TypeError
+# ValueError
+# IndexError
+# KeyError
+# AttributeError
+# ZeroDivisionError
+# FileNotFoundError
+# ImportError
+# MemoryError
+# TimeoutError
---
+++ Day 7/libraries_installation.py
@@ -0,0 +1,12 @@
+# pip install pandas
+# pip install numpy pandas matplotlib seaborn scipy statsmodels
+
+
+import pandas as pd
+import numpy as np
+
+
+import math
+print (math.sqrt (9))
+from math import sqrt
+print(sqrt (9))
